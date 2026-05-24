5436ceaa38dac6c6f35002c5e5d92072fbcf17e3
Ankit Jha <kaunankitjha@gmail.com>
2026-05-24 14:33:50 +0530

    Lets try this

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://kaunankit.github.io"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
